3eb88997f806db002850faafdaf5389181480219
Azimkhon <azeemborz@gmail.com>
2018-04-29 18:12:05 +0500

    React Integration
    Don't clone it I need to finish some parts

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\File' => 'App\Policies\FilePolicy',
+        'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,20 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index') -> name('home');
-
-Route::group(['prefix' => '/account', 'middleware' => ['auth'], 'namespace'=>'Account'],
-function()
-{
-	Route::get('/', 'AccountController@index') -> name('account');
-	
-	Route::group(['prefix' => '/files'], function()
-	{
-		Route::get('/create', 'FileController@create') -> name('account.files.create.start'); 
-		Route::get('/{file}/create', 'FileController@create') -> name('account.files.create');  
-	});
-});
- 
-
-// Route::group(['prefix'=>'admin/{id}'], function()
-// {
-// 	Route::get('page/create', function()
-// 	{ 
-// 		echo "page/create";
-// 	});
-// 	Route::get('page/edit', function()
-// 	{ 
-// 		echo "page/edit";
-// 	});
-// });
+Route::get('/home', 'HomeController@index')->name('home');
