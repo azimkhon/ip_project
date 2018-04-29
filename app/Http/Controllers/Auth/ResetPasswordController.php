d955270ff1e69d60f854f77dd36b86a6c635d8af
Oybek Khakimjanov <oybeka98@mail.ru>
2018-04-29 15:36:16 +0500

    Added User Dashboard

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -27,7 +27,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/account';
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,22 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index') -> name('home');
+
+Route::group(['prefix' => '/account', 'middleware' => ['auth'], 'namespace'=>'Account'],
+function()
+{
+	Route::get('/', 'AccountController@index') -> name('account');
+});
+
+// Route::group(['prefix'=>'admin/{id}'], function()
+// {
+// 	Route::get('page/create', function()
+// 	{ 
+// 		echo "page/create";
+// 	});
+// 	Route::get('page/edit', function()
+// 	{ 
+// 		echo "page/edit";
+// 	});
+// });
