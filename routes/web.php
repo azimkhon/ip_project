e7a6e421fcdc8b762a7daccdb15a90a1cb92026c
oybeka98 <oybeka98@mail.ru>
2018-05-11 17:17:17 +0500

    Accessiblity of /admin link was made secured

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ function()
 	});
 });
  
- Route::group(['prefix' => '/admin', 'namespace' => 'Admin'], function () {
+ Route::group(['prefix' => '/admin', 'namespace' => 'Admin', 'middleware' => ['auth','admin']], function () {
  	Route::get('/', 'AdminController@index') -> name('admin.index');
  });
 
