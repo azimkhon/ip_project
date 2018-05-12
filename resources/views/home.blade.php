ee1da4cf8808e2d6928bbb145244200c0a0b24db
Oybek Khakimjanov <oybeka98@mail.ru>
2018-05-12 18:12:00 +0500

    Landscape of home page is done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,153 @@
 
 @section('content')
 
-    <div class="hero-body">
-        <div class="container has-text-centered">
-            <h1 class="title is-1">
-                Buy and sell files
-            </h1>
+<section class="hero is-primary">
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+              
+              <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+              </ol>
+
+              <div class="carousel-inner">
+                <div class="item active">
+                  <img src="https://img.alltor.me/img/2018-05/12/tbhzx96sm4a7uhicbxnjhjun9.jpg" alt="1" style="width:100%;">
+                </div>
+
+                <div class="item">
+
+                  <img src="https://img.alltor.me/img/2018-05/12/b2p2w6kydalb75n15kbsmdg1n.jpg" alt="2" style="width:100%;">
+                </div>
+              
+                <div class="item">
+                  <img src="https://img.alltor.me/img/2018-05/12/na819sn1eia3lben892x3b95u.jpg" alt="3" style="width:100%;">
+                </div>
+              </div>
+
+             
+              <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+              </a>
         </div>
+</section>
+<section class="section features">
+<div class="container has-text-centered">
+        <div class="title" style="color: #407cc7">
+          Features
+          </div>
+        <br>
+      <div class="columns">
+          <div class="column">
+              <div class="item">
+                <div class="item_text">
+
+                  <div class="fonts">
+                    <i class="fa fa-share-alt fa-4x"></i>
+                  </div>
+                  <br>
+                  File sharing
+                </div>
+              </div>
+          </div>
+          <div class="column">
+              <div class="item">
+                <div class="item_text">
+
+                  <div class="fonts">
+                    <i class="fa fa-archive fa-4x"></i>
+                  </div>
+                  <br>
+                  File storage
+                </div>
+              </div>
+          </div>
+         
+          <div class="column">
+              <div class="item">
+                <div class="item_text">
+
+                  <div class="fonts">
+                    <i class="fa fa-copy fa-4x"></i>
+                  </div>
+                  <br>
+                  File enryption
+                </div>
+              </div>
+          </div>
+
+          <div class="column">
+              <div class="item">
+                <div class="item_text">
+
+                  <div class="fonts">
+                    <i class="fa fa-dollar-sign fa-4x"></i>
+                  </div>
+                  <br>
+                  File deals
+                </div>
+              </div>
+          </div>
+  </div>
+</div>
+
+</section>
+
+<section>
+
+  <img src="https://img.alltor.me/img/2018-05/12/is01ezrlrn8twxlon0fov41al.jpg" alt="1" style="width:100%;">
+
+
+</section>
+
+<section  class="hero is-primary is-medium">
+  <div style="padding-top: 80px;padding-bottom: 80px; background-color: #cacaca;" class="hero-body">
+    <div class="container">
+<div class="columns">
+  <div class="column is-two-fifths">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="{{/contactus">Contact Us</a></li>
+      </ul>
+  </div>
+  <div class="column">
+    <div class="columns">
+    <div class="column">
+      <center>
+        <a href=""><img src="https://img.alltor.me/img/2018-05/12/1iqyelz1d04zfgse8t5xg8tqc.png">
+            <div>
+             Facebook 
+            </div>
+        </a>
+      </center>
     </div>
+    <div class="column">
+      <center>
+        <a href=""><img src="https://img.alltor.me/img/2018-05/12/aoqx1tn0abdl25o1xd75wb2f7.png">
+          <div>
+            Instagram 
+          </div>
+        </a>
+      </center>
+    </div>
+    <div class="column">
+      <center>
+        <a href=""><img src="https://img.alltor.me/img/2018-05/12/pm59379hnv808ai25xs1pmnqe.png">
+          <div>
+           Twitter 
+          </div>
+        </a>
+      </center>
+    </div>
+</div>
+   </div>
+</div>
+</section>
+
 
 @endsection
--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -1,18 +1,59 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    
     @include('layouts.partials._head')
+<style type="text/css">
+    a
+    {
+        text-decoration: none !important;
+    }
+    .navbar {
+    position: relative;
+    min-height: 50px;
+    margin-bottom: 0px !important;
+    border: 1px solid transparent;
+}
+.hero-head{
+    background-color: #f7f7f7;
+    color: #407cc7;
+}
+
+.fa-4x
+{
+    font-size: 4em;
+}
+.item_text
+{
+
+    font-size: 19px;
+}
+.features
+{
+    background-color: white;
+     color: #407cc7 !important;
+}
+.sellblock
+{
+    background-image: url(https://ru.files.fm/images/sell_files_top.jpg);
+    height: auto;
+}
+</style>
+    
 </head>
 <body>
     <div id="app">
-        
-    	<section class="hero is-primary is-large">
-    		<div class="hero-head">
-        		@include('layouts.partials._navigation')
-        	</div>
-        	@yield('content')
-		</section>
-
+        <section class="hero is-primary is-large">
+            <div class="hero-head">
+                @include('layouts.partials._navigation')
+            </div>
+            @yield('content')
+        </section>
     </div>
     
     <!-- Scripts -->
